f09c08cf2f736a3443fd343697723846108344ab
Mateus Gonçalo <mateusgon57@gmail.com>
2019-01-06 15:52:31 -0200

    First controller and Razor Pages

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web";
 
             return View();
         }
