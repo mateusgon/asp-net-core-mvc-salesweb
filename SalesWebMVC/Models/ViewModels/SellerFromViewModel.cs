4633a434196e9a21ae612a7c315e8a351aaf86c4
Mateus Gonçalo <mateusgon57@gmail.com>
2019-01-07 10:52:41 -0200

    Adicionando o SellerFromViewModel

---
+++ SalesWebMVC/Models/ViewModels/SellerFromViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class SellerFromViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Departament> Departaments { get; set; }
+    }
+}
---
+++ SalesWebMVC/Services/DepartamentService.cs
@@ -0,0 +1,19 @@
+﻿using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    private readonly SalesWebMVCContext _context;
+
+    public DepartamentService(SalesWebMVCContext context)
+    {
+        _context = context;
+    }
+
+    public class DepartamentService
+    {
+    }
+}
